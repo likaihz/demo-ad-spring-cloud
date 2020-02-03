bde248704669b0935953322ad4b8a3e4dffb8044
likaihz <lk0219@foxmail.com>
2020-02-03 19:34:33 +0800

    创建广告投放子模块 - 创建项目配置文件pom.xml - 创建应用配置文件application.yml - 更新父模型的项目配置文件 - 更新网关的应用配置文件

---
+++ demo-ad-service/ad-sponsor/src/main/java/cn/likaihz/ad/SponsorApplication.java
@@ -0,0 +1,20 @@
+package cn.likaihz.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author Li Kai
+ */
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableEurekaClient
+@SpringBootApplication
+public class SponsorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SponsorApplication.class, args);
+    }
+}
